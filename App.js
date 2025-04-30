4f3d3b1c2deb022dddc59b8fd8d871c404ff8abd
Tarun Rana <tarunrana1997@gmail.com>
2025-04-30 23:26:19 +0530

    react package instead of cdn

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
-// const heading = React.createElement("h1", { data: "xyz" }, "Hello World again");
-// const heading = React.createElement("div", { data: "xyz" }, [
-//   React.createElement("h1", { data: "xyz" }, "Hello World again"),
-//   React.createElement("h1", { data: "xyz" }, "Hello World again"),
-// ]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading = React.createElement("div", { data: "xyz" }, [
+    React.createElement("h1", { data: "xyz" }, "Hello World again"),
+    React.createElement("h1", { data: "xyz" }, "Hello World again"),
+]);
 
+const jsxheading = 
+    <h1>Hello World <span>rocket</span> </h1>;
 
-const heading = () => {
-    <h1>Hello World</h1>
-  }
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  
-  root.render(<heading/>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxheading);
